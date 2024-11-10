9e2656ab6d8ded4eeb757fc57afb9954ce073894
odwaz <linda.matiwane@gmail.com>
2024-11-10 09:34:29 +0200

    new

--- main.tf
+++ main.tf
@@ -12,40 +12,41 @@ resource "aws_vpc" "main" {
     }
 }
 
-resource "aws_subnet" "public_1" {
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = "10.0.1.0/24"
-    map_public_ip_on_launch = true
+# resource "aws_subnet" "public_1" {
+#     vpc_id            = aws_vpc.main.id
+#     cidr_block        = "10.0.1.0/24"
+#     map_public_ip_on_launch = true
 
-    tags = {
-        Name = "public-subnet-1"
-    }
-}
+#     tags = {
+#         Name = "public-subnet-1"
+#     }
+# }
 
-resource "aws_subnet" "public_2" {
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = "10.0.2.0/24"
-    map_public_ip_on_launch = true
+# resource "aws_subnet" "public_2" {
+#     vpc_id            = aws_vpc.main.id
+#     cidr_block        = "10.0.2.0/24"
+#     map_public_ip_on_launch = true
 
-    tags = {
-        Name = "public-subnet-2"
-    }
-}
+#     tags = {
+#         Name = "public-subnet-2"
+#     }
+# }
 
-resource "aws_subnet" "private_1" {
-    vpc_id     = aws_vpc.main.id
-    cidr_block = "10.0.3.0/24"
+# resource "aws_subnet" "private_1" {
+#     vpc_id     = aws_vpc.main.id
+#     cidr_block = "10.0.3.0/24"
 
-    tags = {
-        Name = "private-subnet-1"
-    }
-}
+#     tags = {
+#         Name = "private-subnet-1"
+#     }
+# }
 
-resource "aws_subnet" "private_2" {
-    vpc_id     = aws_vpc.main.id
-    cidr_block = "10.0.4.0/24"
+# resource "aws_subnet" "private_2" {
+#     vpc_id     = aws_vpc.main.id
+#     cidr_block = "10.0.4.0/24"
 
-    tags = {
-        Name = "private-subnet-2"
-    }
-}
+#     tags = {
+#         Name = "private-subnet-2"
+#     }
+# }
+
